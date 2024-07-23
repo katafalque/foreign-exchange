b7ac34ef21ce35994d5598c6be4da086990a62a0
alperenkarail <alperen@brewww.com>
2024-07-24 00:49:59 +0300

    feature: setup spring boot app.

---
+++ src/main/java/com/example/foreignexchange/ForeignexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.foreignexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignexchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foreignexchange/ForeignexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foreignexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
