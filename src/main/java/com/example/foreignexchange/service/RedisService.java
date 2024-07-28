fb2ee7ab999d94ee237ef08cbc0e7664da655c0b
alperenkarail <alperen@brewww.com>
2024-07-28 04:50:35 +0300

    fix: delete redis service since it'll become unrelated.

--- src/main/java/com/example/foreignexchange/service/RedisService.java
+++
@@ -1,8 +0,0 @@
-package com.example.foreignexchange.service;
-
-
-public interface RedisService {
-    void setValue(String key, String value);
-    String getValue(String key);
-    void deleteValue(String key);
-}
