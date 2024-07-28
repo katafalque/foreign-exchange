c3169a3d01c6bc8fc324486cfef7c8aa7f71dcf7
alperenkarail <alperen@brewww.com>
2024-07-28 17:00:26 +0300

    feature: component ExchangeRateManager to get exchange rates based on the provider in environment

---
+++ src/main/java/com/example/foreignexchange/component/impl/ExchangeRateManagerImpl.java
@@ -0,0 +1,33 @@
+package com.example.foreignexchange.component.impl;
+
+import com.example.foreignexchange.component.ExchangeRateManager;
+import com.example.foreignexchange.data.enums.ExchangeRateProvider;
+import com.example.foreignexchange.provider.exchangerate.BaseExchangeRateProvider;
+import com.example.foreignexchange.service.factory.ExchangeRateProviderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExchangeRateManagerImpl implements ExchangeRateManager {
+    @Value("${exchange.rate.provider}")
+    private String exchangeRateProvider;
+
+    private ExchangeRateProviderFactory exchangeRateProviderFactory;
+
+    @Autowired
+    public ExchangeRateManagerImpl(ExchangeRateProviderFactory exchangeRateProviderFactory){
+        this.exchangeRateProviderFactory = exchangeRateProviderFactory;
+    }
+
+    @Override
+    public double getExchangeRate(String fromCurrency, String toCurrency) {
+        BaseExchangeRateProvider provider = getProvider();
+        return provider.getExchangeRate(fromCurrency, toCurrency);
+    }
+
+    private BaseExchangeRateProvider getProvider(){
+        ExchangeRateProvider exchangeRateProviderEnum = ExchangeRateProvider.fromValue(exchangeRateProvider);
+        return exchangeRateProviderFactory.getService(exchangeRateProviderEnum);
+    }
+}
